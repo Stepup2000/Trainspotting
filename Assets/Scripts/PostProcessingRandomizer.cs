1d3499683d45b50034076a47e9644565741b3b13
Stephan <StephanHC.gibson@gmail.com>
2025-05-07 14:22:55 +0200

    Add PostProcessingRandomizer.
    Added a random for the postprocessing to test out the different profiles.

---
+++ Assets/Scripts/PostProcessingRandomizer.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+
+public class PostProcessingRandomizer : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(ChangeProfileEveryFewSeconds());
+    }
+
+    private IEnumerator ChangeProfileEveryFewSeconds()
+    {
+        while (true)
+        {
+            var dataList = PPController.Instance.postProcessingDataList;
+
+            if (dataList != null && dataList.Count > 0)
+            {
+                int randomIndex = Random.Range(0, dataList.Count);
+                var randomStyle = dataList[randomIndex].style;
+
+                PPController.Instance.SetPostProcessingStyle(randomStyle);
+            }
+
+            yield return new WaitForSeconds(3f);
+        }
+    }
+}
