cacb1723c43597aa0011ee1e20ab8a6138219ceb
Orinap <matiasgomes1400@gmail.com>
2025-05-20 11:52:35 +0200

    Added FMOD bank retrieval system
    Changed FMOD bank location to a folder within unity and created a system that copies banks from the user's fmod studio folder to the unity folder.
    Added 2 files to gitignore so that the path to the files in the fmod studio project remain local, therefore getting rid of the annoying error forcing you to change the path everytime another person pushes changes

---
+++ Assets/Editor/CopyBanks.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class CopyFmodBanks : EditorWindow
+{
+    private static string configPath = "Assets/Editor/FMODBankPathConfig.asset";
+    private static string targetBankPath = "Assets/FMOD_Banks";
+
+    [MenuItem("Tools/FMOD/Copy Bank Files")]
+    public static void CopyBankFiles()
+    {
+        var config = AssetDatabase.LoadAssetAtPath<FMODBankPathConfig>(configPath);
+        if (config == null)
+        {
+            Debug.LogError("FMODBankPathConfig not found. Please create one in Assets/Editor.");
+            return;
+        }
+
+        string sourceBankPath = config.sourceBankPath;
+        if (!Directory.Exists(sourceBankPath))
+        {
+            Debug.LogError($"FMOD bank path does not exist: {sourceBankPath}");
+            return;
+        }
+
+        if (!Directory.Exists(targetBankPath))
+            Directory.CreateDirectory(targetBankPath);
+
+        var bankFiles = Directory.GetFiles(sourceBankPath, "*.bank", SearchOption.TopDirectoryOnly);
+
+        foreach (var file in bankFiles)
+        {
+            var fileName = Path.GetFileName(file);
+            var dest = Path.Combine(targetBankPath, fileName);
+            File.Copy(file, dest, true);
+            Debug.Log($"Copied: {fileName}");
+        }
+
+        AssetDatabase.Refresh();
+        Debug.Log("FMOD bank copy complete.");
+    }
+}
---
+++ Assets/Editor/FMODBankPathConfig.cs
@@ -0,0 +1,9 @@
+// FMODBankPathConfig.cs
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FMODBankPathConfig", menuName = "FMOD/Bank Path Config")]
+public class FMODBankPathConfig : ScriptableObject
+{
+    public string sourceBankPath;
+}
+
